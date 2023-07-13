f209d77c795a9483d1a0c2b87566ebe35eeeda45
puthiye <38553241+puthiye@users.noreply.github.com>
2023-07-13 20:37:47 +1000

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,45 +3,39 @@ import os
 import subprocess
 from google.cloud import storage
 
-#delay in minutes
-off_delay=5 
+#delay in seconds
+off_delay=300
 
 # service account key credentials file path
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r'sa-bucket-key.json'
 
 def download_cs_file(bucket_name, file_name, destination_file_name):
     storage_client = storage.Client()
-
     bucket = storage_client.bucket(bucket_name)
-
     blob = bucket.blob(file_name)
     blob.download_to_filename(destination_file_name)
 
 
 def upload_cs_file(bucket_name, source_file_name, destination_file_name):
     storage_client = storage.Client()
-
     bucket = storage_client.bucket(bucket_name)
-
     blob = bucket.blob(destination_file_name)
     blob.upload_from_filename(source_file_name)
 
 
 def is_gate_open():
-
     #if status file has open, return true
     download_cs_file("swann-config", "status", "tmp")
     with open('tmp') as f:
          file_content = f.read().splitlines()
-
     if "open" in file_content:
         return True
     else:
         return False
 
-
 def main():
 
+    was_disabled=False
     print('INFO::: starting swann_gate app...')
 
     while True:
@@ -51,11 +45,16 @@ def main():
             subprocess.call(['sh', './eth1_enable.sh'])
             #once opened, reset the status by writing close to bucket file
             upload_cs_file("swann-config", "status_closed", "status")
+            was_disabled=False
 
-        else:
+        elif not was_disabled:
             time.sleep(off_delay)
             print('INFO::: disabling swann external access..')
             subprocess.call(['sh', './eth1_disable.sh'])
+            was_disabled=True
+
+        else:
+            print('INFO::: eth1 was already disabled, no action required')
 
 
 
